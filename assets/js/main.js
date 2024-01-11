3466e6b6d547c1f0c47bfb4b15f4228fb3c30925
philipkuerzel <philipkuerzel@gmail.com>
2024-01-11 16:57:27 +0100

    js mit resetButton und kein Auslösen bei Unentschieden

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,8 @@
-
-
 const result = document.querySelector("#result")
 const playerScore = document.querySelector("#playerScore")
 const computerScore = document.querySelector("#computerScore")
-let yourScore  = 0
-let hisScore = 0
-
 const playerResult = document.querySelector("#playersResult")
 const computerResult = document.querySelector("#computerResult")
-
 const rundenanzeige = document.querySelector("#rundenanzeige")
 
 const schereBtn = document.querySelector("#schere")
@@ -18,7 +12,9 @@ const papierBtn = document.querySelector("#papier")
 const reset =  document.querySelector("#reset")
 
 let start = 0
-let counter = 1
+let counter = 0
+let yourScore  = 0
+let hisScore = 0
 
 let rundenType = document.querySelector("input[type=radio]:checked")
 
@@ -39,32 +35,35 @@ const game = (playersChoice) => {
     let computerChoice = computer()
     if (start < rundenType) {
         
-        rundenanzeige.innerHTML = `<h2>${counter++} / ${rundenType}</h2>`
-
         if (computerChoice === playersChoice) {
-            start++
             result.textContent = "Draw"
             playerResult.textContent = `${playersChoice}`
             computerResult.textContent = `${computerChoice}`
+            rundenanzeige.innerHTML = `<h2>${counter} / ${rundenType}</h2>`
         } else if ((computerChoice === "schere" && playersChoice === "stein") || (computerChoice === "stein" && playersChoice === "papier") || computerChoice === "papier" && playersChoice === "schere") {
             start++
             result.textContent = "You win this round"
             yourScore++
+            counter++
             playerScore.textContent =`${yourScore}`
             playerResult.textContent = `${playersChoice}`
             computerResult.textContent = `${computerChoice}`
+            rundenanzeige.innerHTML = `<h2>${counter} / ${rundenType}</h2>`
         } else if ((computerChoice === "stein" && playersChoice === "schere") || (computerChoice === "papier" && playersChoice === "stein") || computerChoice === "schere" && playersChoice === "papier") {
             start++
+            counter++
             result.textContent = "You lose this round"
             hisScore++
             computerScore.textContent =`${hisScore}`
             playerResult.textContent = `${playersChoice}`
             computerResult.textContent = `${computerChoice}`
+            rundenanzeige.innerHTML = `<h2>${counter} / ${rundenType}</h2>`
         }
     }
-
 }
 
 schereBtn.addEventListener("click", () => game("schere"))
 steinBtn.addEventListener("click", () => game("stein"))
 papierBtn.addEventListener("click", () => game("papier"))
+
+reset.addEventListener("click", () => location.reload())
