64b4b39c7ad7c509d4bcbbf32157dab60416d99d
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-11 14:45:44 +0100

    noch einmal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 const computer = Math.floor(Math.random() * 3) +1
 console.log(computer);
 
-
 const schereBtn = document.querySelector("#schere")
 const steinBtn = document.querySelector("#stein")
 const papierBtn = document.querySelector("#papier")
 
 const reset =  document.querySelector("#reset")
-
 
 let start = 0
 
