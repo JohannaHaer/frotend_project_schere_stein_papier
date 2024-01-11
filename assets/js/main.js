423be6e0a9f1d37321e9a02686a415c5a8bc9dc7
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-11 14:45:32 +0100

    noch einmal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 const computer = Math.floor(Math.random() * 3) +1
 console.log(computer);
-let schere, stein, papier
 
-
-let roundName = document.getElementsByName("runden")
 
 const schereBtn = document.querySelector("#schere")
 const steinBtn = document.querySelector("#stein")
@@ -12,12 +9,6 @@ const papierBtn = document.querySelector("#papier")
 const reset =  document.querySelector("#reset")
 
 
-const game = (runden) => {
-    for(let i = 0; i<roundName.length; i++){
-        if(roundName[i].checked){
-    
-            console.log(runden);
-        }
-    }console.log(roundName.value);
-}
+let start = 0
 
+let rundenType = document.querySelector("input[type=radio]:checked")
